733ca49020914b1edc89294bee9339b7f5ee906f
Shayne Boyer <shayne.p.boyer@disney.com>
2015-09-30 22:08:51 -0400

    adding children service

---
+++ src/child-service.ts
@@ -0,0 +1,45 @@
+import {Http} from 'angular2/http';
+import {Child} from './child';
+
+export class ChildService {
+    children: Child[] = [];
+
+    constructor(private _http: Http) {}
+
+	getCurrentChildren(): Rx.IPromise<Child[]> {
+		this.children.length = 0;
+		let promise = this._http.get('url')
+			.toRx().map((response: any) => response.json()).toPromise()
+			.then(children => {
+				this.children.push(...children);
+				return this.children;
+			})
+			//TODO: fix catch
+			//.catch(e => this._fetchFailed(e)) // want we want to say
+			// baroque way to ensure promise stays Promise<Hero[]>
+			.then<Child[]>(_ => _, e => this._fetchFailed(e));
+		return promise;
+	}
+
+	private _fetchFailed(error:any) {
+		console.error(error);
+		return Promise.reject(error);
+	}
+
+
+    getChild(caseNumber: string){
+        return Promise.resolve(ITEMS)
+        .then((children) => { return children.filter((c) => {
+				return c.caseNumber === caseNumber;
+			})[0]});
+    }
+
+    getChildByState(state: string){
+        return Promise.resolve(ITEMS)
+        .then((children) => { return children.filter((c) => {
+				return c.state === state;
+			})[0]});
+    }
+}
+
+var ITEMS : Child[] = [];
---
+++ src/child.ts
@@ -0,0 +1,17 @@
+export interface Child {
+    name: string;
+    ageNow: number;
+    caseType: string;
+    state: string;
+    dateReported: Date;
+    missingFrom: string;
+    contactName: string;
+    contactPhone: string;
+    photos: string[];
+    photoC: string[];
+    photoX: string[];
+    abductor1: string;
+    abductor2: string;
+    poster: string;
+    caseNumber: string;
+}
