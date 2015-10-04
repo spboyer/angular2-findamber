3077c40e785cc8fe888f484479ed60bf668b1f7c
Shayne Boyer <shayne.p.boyer@disney.com>
2015-10-04 16:42:31 -0400

    updates for alpha38

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import {View, Component} from 'angular2/angular2';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+import {CharactersComponent} from './characters.component';
+import {DashboardComponent} from './dashboard.component';
+
+@Component({ selector: 'my-app' })
+@View({
+  template: `
+    <a [router-link]="['./Dashboard']">Dashboard</a>
+    <a [router-link]="['./Characters']">Characters</a>
+    <router-outlet></router-outlet>
+    `,
+  directives: [ROUTER_DIRECTIVES]
+})
+@RouteConfig([
+  { path: '/', as: 'Dashboard', component: DashboardComponent },
+  { path: '/characters', as: 'Characters', component: CharactersComponent }
+])
+export class AppComponent { }
