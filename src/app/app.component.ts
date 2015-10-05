a1323b789ed4a0a8b3bbf418677f632586febd65
Boyer, Shayne P <Shayne.P.Boyer@disney.com>
2015-10-05 14:35:16 -0400

    Component BeTogether

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import {View, Component} from 'angular2/angular2';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {CharactersComponent} from './characters.component';
 import {DashboardComponent} from './dashboard.component';
+import {BeTogetherComponent} from './be.together.component';
 
 @Component({ selector: 'my-app' })
 @View({
   templateUrl: 'app/app.component.html',
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES, BeTogetherComponent]
 })
 @RouteConfig([
   { path: '/', as: 'Dashboard', component: DashboardComponent },
---
+++ src/app/be.together.component.ts
@@ -0,0 +1,12 @@
+import {Component, View} from 'angular2/angular2';
+
+
+@Component({ selector: 'be-together' })
+@View({
+  templateUrl: 'app/be.together.html',
+  styles: [`
+  `]
+})
+export class BeTogetherComponent {
+
+}
